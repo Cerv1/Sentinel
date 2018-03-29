2cb9fecea5ba23fc5bac68840e8ff8193cab1036
Sergio Cervilla <cervick13@gmail.com>
2018-03-29 13:49:47 +0200

    Starting with the Sentinel application

---
+++ app/src/main/java/dev/cervi/sentinel/PopUpActivity.java
@@ -0,0 +1,13 @@
+package dev.cervi.sentinel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up);
+    }
+}
